2e0302cf3f5828c9bc09e40966c311a6733a139a
andreas <andreas.arnolfo@gmail.com>
2018-12-24 16:50:21 +0100

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAc9Tt8pQQF3ntu2Tz66iUKl_fXHtdhIyc',
-    authDomain: 'firestarter-96e46.firebaseapp.com',
-    databaseURL: 'https://firestarter-96e46.firebaseio.com',
-    projectId: 'firestarter-96e46',
-    storageBucket: 'firestarter-96e46.appspot.com',
-    messagingSenderId: '581326886241'
+    apiKey: 'AIzaSyBOIBBramGD6HrNF1ZafndU_LZUpFoImfQ',
+    authDomain: 'lily-7eb4b.firebaseapp.com',
+    databaseURL: 'https://lily-7eb4b.firebaseio.com',
+    projectId: 'lily-7eb4b',
+    storageBucket: 'lily-7eb4b.appspot.com',
+    messagingSenderId: '471451077647'
   }
 };
 
